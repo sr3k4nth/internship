6c8a8c7cb92507ae14a0f4f03aa2c3307201ad73
Lenovo <srikanth.tnrs@gmail.com>
2022-11-09 17:54:19 +0530

    minor bug fixes

--- src/routes/fxRoutes.js
+++ src/routes/fxRoutes.js
@@ -11,9 +11,7 @@ const router = express.Router();
 
 // Sample URL: http://localhost:8080/fx-manager/rates/dollar?date=2013-01-23T18:30:00.000Z%22
 router.get("/rates/dollar", async (req, res) => {
-  // const { destCurrencyCode, fromDate, toDate } = req.params;
-
-  const date = req.params.date || "2013-01-23T18:30:00.000Z";
+  const date = req.query.date || "2013-01-23T18:30:00.000Z";
 
   const payload = {
     date,
@@ -31,7 +29,7 @@ router.get("/rates/dollar", async (req, res) => {
     const obj = {
       currencyName: item.currencyName,
       currencyCode: item.currencyCode,
-      valueInDollar: (1 / item.exchangeRate).toFixed(3),
+      valueInDollar: +(1 / +item.exchangeRate).toFixed(4),
     };
     mapresult.push(obj);
     if (errorMessage) {
@@ -43,19 +41,19 @@ router.get("/rates/dollar", async (req, res) => {
   res.json(apiResult);
 });
 
-// GET  http://localhost:8080/fx-manager/rates?srcCurrencyCode=USD&destCurrencyCode=GBP&fromDate=2013-01-19T00:00:00.000+0000&toDate="2013-12-20T00:00:00.000+0000"
+// GET  http://localhost:8080/fx-manager/rates?srcCurrencyCode=USD&destCurrencyCode=GBP&fromDate=2013-01-19T00:00:00.000+0000&toDate=2013-12-20T00:00:00.000+0000
 
-router.get("/rates", async (req, res) => {
+router.get("/rates", async (req, res, next) => {
   try {
-    const { destCurrencyCode, fromDate, toDate } = req.params;
+    const { destCurrencyCode, fromDate, toDate } = req.query;
     const payload = {
-      currencyCode: destCurrencyCode || "DKK",
+      currencyCode: destCurrencyCode,
       date: {
-        $gt: fromDate || "2013-01-19T00:00:00.000+0000",
-        $lt: toDate || "2013-12-20T00:00:00.000+0000",
+        $gt: fromDate,
+        $lt: toDate,
       },
     };
-    const dbResult = await RatesModel(payload);
+    const dbResult = await RatesModel.find(payload);
 
     const getHighest = Math.max(...dbResult.map((val) => val.exchangeRate));
     const getLowest = Math.min(...dbResult.map((val) => val.exchangeRate));
@@ -84,7 +82,7 @@ router.get("/rates", async (req, res) => {
   }
 });
 
-router.get("/currency-list", async (req, res) => {
+router.get("/currency-list", async (req, res, next) => {
   try {
     const dbResult = await CurrencyModel.find({});
 
